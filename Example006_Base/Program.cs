3509b010310849c4a3e9fa4ab281595da403f851
PavelKRY <kryukov.p14@gmail.com>
2022-08-18 16:28:12 +0500

    Base project

---
+++ Example006_Base/Program.cs
@@ -0,0 +1,13 @@
+﻿int a = 1;
+int b = 5;
+int c = 345;
+int d = 123;
+int e = 12444321;
+int max = a;
+if (a > max) max = a;
+if (b > max) max = b;
+if (c > max) max = c;
+if (d > max) max = d;
+if (e > max) max = e;
+Console.Write("max = ");
+Console.WriteLine(max);
